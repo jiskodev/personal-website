a911dd04614e93235b0378cb2eb6252d87fb1da9
jiskodev <sergio.jisko@gmail.com>
2020-11-28 15:52:29 +0100

    fixed nav on phone

--- src/components/NavMenu/navMenuElements.js
+++ src/components/NavMenu/navMenuElements.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 export const Container = styled(motion.div)`
     width: 100%;
     height: 100vh;
+    min-height: -webkit-fill-available;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
