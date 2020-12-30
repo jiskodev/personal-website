e00c05b19d918267610a7a0127c548724f7d286c
jiskodev <sergio.jisko@gmail.com>
2020-12-30 08:00:47 +0100

    removed textScramble

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Container, Hello, Image, Text, Wrapper, ImageWrapper, Box } from './introElements'
-import TextScramble from '@twistezo/react-text-scramble'
 import Memoji from '../../assets/memoji.png'
 
 
-const texts = [
-    "A self-taught Front End Developer"
-]
+
 
 function Intro() {
     return (
@@ -18,7 +15,7 @@ function Intro() {
                         <Hello animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y : -100 }} transition={{ duration: 0.8 }}>
                             Hello,<br /> I'm Sergio
                         </Hello>
-                        <Text><TextScramble texts={texts}/></Text>
+                        <Text>A self-taught Front End Developer</Text>
                     </Box>
                     <ImageWrapper animate={{ scale: 1 }} initial={{ scale: 0 }} transition={{ duration: 0.8 }}>
                         <Image src={Memoji}></Image>
